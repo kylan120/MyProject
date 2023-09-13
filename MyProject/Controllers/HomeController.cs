3b9f0c5f97ade4c6b9e4245c047b0f3db0cc00fc
Kylan Larson <kylan.larson@southeasttech.edu>
2023-09-13 15:15:24 -0500

    I added comments

--- MyProject/Controllers/HomeController.cs
+++ MyProject/Controllers/HomeController.cs
@@ -5,10 +5,12 @@ namespace MyProject.Controllers
 {
     public class HomeController : Controller
     {
+        // This is the controller for index
         public IActionResult Index()
         {
             return View();
         }
+        // This is the controller for display
         public IActionResult display(User model)
         {
             return View(model);
--- MyProject/Models/User.cs
+++ MyProject/Models/User.cs
@@ -4,6 +4,7 @@ namespace MyProject.Models
 {
     public class User
     {
+        // This will have the be required to Enter your name as email
         [Required(ErrorMessage = "Please Enter a name")]
         public string? Name { get; set; }
         [Required(ErrorMessage = "Please Enter your email")]
